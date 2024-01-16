90796cf605c7ec2646e9c573bedd4222da783329
pravin shelake <pravinshelake1997@gmail.com>
2024-01-16 21:31:49 +0530

    My first transaction

---
+++ Account.py
@@ -0,0 +1,38 @@
+class Account:
+    def __init__(self,initial_deposite):
+        self.name = input("Enter your name:")
+        self.initial_deposit = 0
+        self.transaction = [ ]
+        self.transaction_history=[ ]
+        self.emailid=""
+        self.pincode=[]
+        self.id = [ ]
+        self.activity()
+
+    def activity(self):
+        user_input = input("""Welcome to the bank.... Now how would you like to proceed?
+                        1. Enter 1 to withdraw 
+                        2. Enter 2 to deposite
+                        3. Enter 3 to checkbalance
+                        4. Enter 4 to displaytransactionhistory
+                        5. Enter 5 to history
+                        6. Enter 6 to createaccount
+                        7. Enter 7 to exit
+        """)
+        if user_input == "1":
+            self.withdraw(1)
+        elif user_input == "2":
+            self.deposite()
+        elif user_input == "3":
+            self.check_balance()
+        elif user_input == "4":
+            self.display_transaction_history()
+        elif user_input == "5":
+            self.transaction_history()
+        elif user_input == "6":
+            self.create_account()
+
+        else:
+            print("Exit")
+        
+
---
+++ main.py
@@ -0,0 +1,13 @@
+from BankingSystem.BankingSystem import BankingSystem
+from BankingSystem.Account import Account
+
+
+print ("Welcome to the bank")
+
+print("Create Your account")
+
+print(B = BankingSystem(1200))
+
+
+
+
